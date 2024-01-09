9ef877f7ae3f18bb89309778ca4cf73f13d42130
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2024-01-09 11:24:03 +0200

    Adjusted files in components folder

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { setWithExpiry } from '../utils/localStorageUtils';
+
+function Login() {
+    return(
+        <div className='relative'>
+
+        </div>
+    );
+}
+
+export default Login;
